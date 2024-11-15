dc0bcdd5d581f0a8c0c0da789f93aa40d19a1bcd
Zeyy <zaidanagusta319@gmail.com>
2024-11-15 23:06:40 +0000

    mojito : add missing letters

--- TenXOS.sh
+++ TenXOS.sh
@@ -9,7 +9,7 @@ repo init -u https://github.com/TenX-OS/manifest.git -b fourteen --git-lfs
 # cloning DT
 # device tree
 git clone https://github.com/TenX0/device_xiaomi_mojito.git --depth 1 -b mojito-universe device/xiaomi/mojito
-git clone https://github.com/TenX0/android_device_xiaomi_sm6150-common.git --depth 1-b mojito-universe evice/xiaomi/sm6150-common
+git clone https://github.com/TenX0/android_device_xiaomi_sm6150-common.git --depth 1-b mojito-universe device/xiaomi/sm6150-common
 # kernel tree
 git clone https://github.com/TenX0/kernel_xiaomi_mojito.git --depth 1 -b inline-rom kernel/xiaomi/mojito
 # vendor tree
